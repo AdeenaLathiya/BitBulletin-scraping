c356178cf2121a85babd7ec76c66066b742f19f8
AdeenaLathiya <adeenalathiya@gmail.com>
2022-03-01 18:55:06 +0500

    express tribune business

---
+++ expressTribune/expressTribune_business/expressTribune_business/items.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ExpresstribuneBusinessItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    title = scrapy.Field()
+    excerpt = scrapy.Field()
+    author = scrapy.Field()
+    date = scrapy.Field()
+    image = scrapy.Field()
+    storyLocation = scrapy.Field()
+    story = scrapy.Field()
+
+    
+    pass
